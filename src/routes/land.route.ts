3017b8e6915967829fc31f8af938299028ae8d0f
GarrachFares <faresgarrach@insat.u-carthage.tn>
2022-03-26 21:32:47 +0100

    fixing :id and things

--- src/routes/land.route.ts
+++ src/routes/land.route.ts
@@ -2,17 +2,12 @@ import { Router } from "express";
 import  * as lands from "../controllers/land.controller";
 import { authentify } from "../middleware/authentify";
 
-// import * as groups from '../controllers/groups.controller'
-// import { authentify } from "../middlewares/authentify";
-// import { userIsAdmin } from "../middlewares/userIsAdmin";
-
-
 const router = Router()
 
 router.get('/' ,lands.getAll) //only in dev
-router.get('/my' ,authentify('LANDLORD'),lands.getByMyId) 
+router.get('/my' ,authentify('LANDLORD'),lands.getByMyId) //get lands (get by id of user li fil token)
 router.get('/all' ,lands.getFreeModLands)
-router.get('/id' ,lands.getById)
-router.post('/' ,authentify('LANDLORD'),lands.create)
+router.get('/:id' ,lands.getById)
+router.post('/' ,authentify('LANDLORD'),lands.create) //create land (user from token) # LANDLORD
 
 export default router
